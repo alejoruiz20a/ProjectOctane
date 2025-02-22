8b40df3b910fb3c520e145c71a886deb8728db5e
alejoruiz20a <aleamador2003@gmail.com>
2025-02-22 11:19:45 -0500

    0.15 Ahora puedes elegir el carro que quieres comprar con el id, pero no se está guardando en la base de datos.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # ProjectOctane
+
+## ANTES DE EJECUTAR START_DB.PY
+- Crear una base de datos en postgresql
+- Crear un archivo en la raiz del proyecto llamado .env
+- Configurar las variables de entorno: DB_NAME, DB_HOST, DB_PASSWORD, KEY y DB_USER
+
+## COMANDOS
+- pip install psycopg2-binary
+- pip install discord.py
+- pip install dotenv
--- main.py
+++ main.py
@@ -21,8 +21,6 @@ intents.message_content = True  # Permite leer mensajes en servidores
 
 # CLIENTE
 bot = commands.Bot(command_prefix = "!", intents = intents)
-#FDFSDFSDFSDF
-
 
 @bot.command()
 async def register(ctx):
@@ -67,27 +65,24 @@ async def car_store(ctx):
         await ctx.send("Lo sentimos, ha habido un error, intentalo de nuevo más tarde.")
 
 @bot.command()
-async def buy(ctx, *car_name):
+async def buy_car(ctx, *car_name):
     if not car_name:
-        await ctx.send("You have to type !buy [brand] [name] [year]")
+        await ctx.send("You have to type !buy_car [id]")
         return
 
-    await ctx.send(f"Searching for the {" ".join(car_name)}...")
+    await ctx.send(f"Searching for the car with the id: {" ".join(car_name)}...")
 
     conn = None
     try:
         conn = get_connection()
         cursor = conn.cursor()
-        cursor.execute(queries["getCarModelByName"],(car_name[0], car_name[1], car_name[2]))
+        cursor.execute(queries["getCarModelById"],(car_name[0],))
         car_model = cursor.fetchone()
         
-        await ctx.send(f"Buying the {type(car_model)}")
+        await ctx.send(f"Buying the {car_model[1]} {car_model[2]} {car_model[3]}...")
+        await ctx.send(f"All done! the {car_model[1]} {car_model[2]} {car_model[3]}  is all yours. Enjoy!")
     except Exception as e:
         await ctx.send(f"An error has ocurred... {e}")
-
-
-
-
 
 # Iniciar el bot
 bot.run(TOKEN)
